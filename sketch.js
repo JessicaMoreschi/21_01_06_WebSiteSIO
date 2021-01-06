2323102f53ec5fdcedef8ee2fc00b8103197e15f
JessicaMoreschi <jessica.moreschi98@gmail.com>
2021-01-06 21:23:04 +0100

    type

--- sketch.js
+++ sketch.js
@@ -2,13 +2,3 @@ document.getElementById('demosMenu').addEventListener('change', function(e){
     var dropdown = document.getElementById('demosMenu');
     window.location.href = dropdown.options[dropdown.selectedIndex].getAttribute('id') + '.html';
 });
-
-function buy(){
-  document.getElementById('testoBuy').style.display='block';
-  var btn = document.getElementById("testoBuyBtn");
-  btn.innerHTML = "Comprato";
-  btn.style.backgroundColor='#f9f9f8';
-  btn.style.borderColor='#887b86';
-  btn.style.color='#887b86';
-
-}
