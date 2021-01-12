280cd6b03a0801b5051e76165f4a3db17fbcf8c7
JessicaMoreschi <jessica.moreschi98@gmail.com>
2021-01-12 21:29:20 +0100

    slider ok

--- public/sketchSlider.js
+++ public/sketchSlider.js
@@ -1,31 +1,20 @@
-var sketchSlider = function(s) {
+// var sketchSlider = function(s) {
 
   var myCanvas
-  var slider1;
-  var slider2;
-  var slider3
-  var cursor
+  var slider1V;
+  var slider2V;
+  var slider3V;
 
-  s.setup = function() {
-    slider1 = s.createSlider(0, 255, 127.5);
-    slider1.parent('slider1');
-    slider2 = s.createSlider(0, 255, 127.5);
-    slider2.parent('slider2');
-    slider3 = s.createSlider(0, 255, 127.5);
-    slider3.parent('slider3');
 
-    cursor=document.getElementById('slider1').style=""
+
+  function draw() {
+    slider1V = document.getElementById("slider1").value;
+    slider2V = document.getElementById("slider2").value;
+    slider3V = document.getElementById("slider3").value;
   };
 
 
-  s.draw = function() {
-    let r = slider1.value();
-    let g = slider2.value();
-    let b = slider3.value();
 
-  };
+// }
 
-
-}
-
-var myp5 = new p5(sketchSlider);
+// var myp5 = new p5(sketchSlider);
